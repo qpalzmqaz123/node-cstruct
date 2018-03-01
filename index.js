6abbcdb75c948663cf13556a5e30d9c24f41ed26
wangqj <qpalzmqaz123@gmail.com>
2018-03-01 17:08:47 +0800

    add array

--- index.js
+++ index.js
@@ -1,39 +1,16 @@
 'use strict'
 
-const StructConfig = require('./lib/config')
 const BaseType = require('./lib/base')
 const IntType = require('./lib/int')
 const StructType = require('./lib/struct')
 
 class Struct {
-  constructor () {
-    this.$_config = new StructConfig()
-  }
-
   get int () {
-    const self = this
-
-    return class extends IntType {
-      constructor (number) {
-        super({
-          config: self.$_config
-        }, number)
-      }
-
-      static get byteSize () {
-        return self.$_config.dataSize['int']
-      }
-    }
+    return IntType
   }
 
   get struct () {
-    const self = this
-
-    return class extends StructType {
-      constructor (value) {
-        super(self.$_config, value)
-      }
-    }
+    return StructType
   }
 
   get union () {
--- test/index.js
+++ test/index.js
@@ -44,6 +44,21 @@ describe('test int', () => {
   })
 })
 
+describe('test array', () => {
+  it('test int', () => {
+    const arr = new struct.int.times(3)
+
+    for (let i = 0; i < arr.length; i++) {
+      arr[i].$value = i + 1
+    }
+
+    assert(Buffer.compare(
+      arr.$buffer,
+      Buffer.from('010000000200000003000000', 'hex')
+    ) === 0)
+  })
+})
+
 describe('struct', () => {
   it('test base', () => {
     const point = struct.struct([
